acd24f2cc9e6a23ed3c516d059a3bd42470d9676
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2020-12-14 17:07:26 -0600

    added list of licenses to choices options

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const questions = [
         name: 'license',
         type: 'list',
         message: 'Which license applies to this project?',
-        choices: ['MIT', 'None']
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT', "BSD 2-Clause 'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License", 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense', 'None']
     },
     {
         name: 'contributing',
